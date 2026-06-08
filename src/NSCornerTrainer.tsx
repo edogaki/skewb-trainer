6ce691faacf36160b21d5b7de992d4ae41d6c577
edogaki <225130043+edogaki@users.noreply.github.com>
2026-06-08 15:01:13 +0800

    Add NS Corner Trainer

--- src/NSCornerTrainer.tsx
+++ src/NSCornerTrainer.tsx
@@ -1,28 +1,18 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import SkewbRenderer from './SkewbRenderer';
-import { type NSCenterTrainerState, nonWhiteColors, nsCenterTrainerStateToSkewbState, nsCenterTrainerStateToCenterPerm, CenterPerm, type Options } from './skewbUtils';
+import { nsCornerOrientations, type NSCornerTrainerState, nsCornerTrainerStateToCornerOrientation, CornerOrientation, nsCornerTrainerStateToSkewbState } from './skewbUtils';
 import { CubeRotation } from './utils';
 
 import correctSound from "./sounds/correct.mp3";
 import wrongSound from "./sounds/wrong.mp3";
-import { bindKeysToCenterPerm } from './keyboardShortcuts';
-import OptionsEditor from './OptionsEditor';
 
-function shuffleArray(array: unknown[]) {
-    for (let i = array.length - 1; i > 0; i--) {
-        const j = Math.floor(Math.random() * (i + 1));
-        [array[i], array[j]] = [array[j], array[i]];
-    }
-}
-
-function generateNSCenters() {
-    const centers = nonWhiteColors.slice();
-    shuffleArray(centers);
+function generateNSCorners() {
+    const corners = Object.keys(nsCornerOrientations)[Math.floor(Math.random() * Object.keys(nsCornerOrientations).length)].split("|")
     const randomRotation = CubeRotation[Math.floor(Math.random() * CubeRotation.length)];
-    const state: NSCenterTrainerState = {
-        centers: centers.slice(0, 3),
+    const state: NSCornerTrainerState = {
+        corners: corners,
         rotation: randomRotation,
-    } as NSCenterTrainerState
+    } as NSCornerTrainerState;
     return state;
 }
 
@@ -35,16 +25,14 @@ function play(audio: HTMLAudioElement) {
 }
 
 
-function NSCenterTrainer() {
-    const [options, setOptions] = useState<Options>({
-        showRightCornerColors: true,
-    });
-    const [nsCenterState, setNSCenterState] = useState(generateNSCenters());
-    const centerPerm = nsCenterTrainerStateToCenterPerm(nsCenterState);
-    
+function NSCornerTrainer() {
+    const [nsCornerState, setNSCornerState] = useState(generateNSCorners());
+    const cornerOrientation = nsCornerTrainerStateToCornerOrientation(nsCornerState);
+    console.log(nsCornerState);
+
     const isErrorButtonInitialState = Object.fromEntries(
-        Object.keys(CenterPerm).map(k => [k, false])
-    ) as Record<keyof typeof CenterPerm, boolean>;
+        Object.keys(CornerOrientation).map(k => [k, false])
+    ) as Record<keyof typeof CornerOrientation, boolean>;
 
     const [isErrorButton, setIsErrorButton] = useState(isErrorButtonInitialState);
     
@@ -52,13 +40,12 @@ function NSCenterTrainer() {
     const [totalQuestions, setTotalQuestions] = useState(0);
     
     function newState() {
-        setNSCenterState(generateNSCenters());
+        setNSCornerState(generateNSCorners());
         setIsErrorButton(isErrorButtonInitialState)
     }
 
-    async function selectCenterPerm(k: keyof typeof CenterPerm) {
-        console.log({ k, centerPerm });
-        if (CenterPerm[k] === centerPerm) {
+    async function selectCornerOrientation(k: keyof typeof CornerOrientation) {
+        if (CornerOrientation[k] === cornerOrientation) {
             play(correctAudio);
             if (Object.values(isErrorButton).every((v) => v === false)) {
                 setCorrectQuestions((q) => q + 1)
@@ -74,14 +61,16 @@ function NSCenterTrainer() {
         }
     }
 
+    /*
     useEffect(() => {
         const cleanupFunc = bindKeysToCenterPerm(selectCenterPerm);
         return cleanupFunc;
     }, [centerPerm, isErrorButton])
+    */
 
     return (
         <>
-            <h2>NS Center Trainer</h2>
+            <h2>NS Corner Trainer</h2>
             <div className="trainer">
                 <div className="trainer-left">
                     <div>
@@ -93,33 +82,26 @@ function NSCenterTrainer() {
                             setTotalQuestions((q) => q + 1);
                         }
                         setIsErrorButton((obj) => { return Object.fromEntries(
-                            (Object.keys(obj) as Array<keyof typeof CenterPerm>).map((k) => CenterPerm[k] === centerPerm ? [k, false] : [k, true])
-                        ) as Record<keyof typeof CenterPerm, boolean> });
+                            (Object.keys(obj) as Array<keyof typeof CornerOrientation>).map((k) => CornerOrientation[k] === cornerOrientation ? [k, false] : [k, true])
+                        ) as Record<keyof typeof CornerOrientation, boolean> });
                     }}>I give up</button>
-                    <SkewbRenderer state={nsCenterTrainerStateToSkewbState(nsCenterState, options)}/>
+                    <SkewbRenderer state={nsCornerTrainerStateToSkewbState(nsCornerState)}/>
                 </div>
                 <div className="trainer-right">
-                    {(Object.keys(CenterPerm) as Array<keyof typeof CenterPerm>).map(k => (
+                    {(Object.keys(CornerOrientation) as Array<keyof typeof CornerOrientation>).map(k => (
                         <div key={k}>
                             <button
                                 className={`${isErrorButton[k] && "error"} `}
-                                onClick={() => selectCenterPerm(k)}
+                                onClick={() => selectCornerOrientation(k)}
                             >
-                                {CenterPerm[k]}
+                                {CornerOrientation[k]}
                             </button>
                         </div>
                     ))}
                 </div>
             </div>
-            <a target="_blank" href="https://docs.google.com/spreadsheets/d/1HcICTLEa15KYq-9FwdQqencTq9m9xHZiSsSZxGsRoTk">
-                My SA/NS center recognition doc that lists all possible cases and easy-to-mistake ones
-            </a>
-            <p>
-                Keybinds: Space for Pure/Solved, A for Swirl, S for Wat, D for X perm, F for Horizontal U perm, and so on.
-            </p>
-            <OptionsEditor options={options} setOptions={setOptions} />
         </>
     )
 }
 
-export default NSCenterTrainer;
+export default NSCornerTrainer;
